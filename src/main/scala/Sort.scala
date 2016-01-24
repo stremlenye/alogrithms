76e41b3e61b7fb11b3bb6c31fb57cc4e76d34cb7
stremlenye <stremlenye@gmail.com>
2016-01-25 00:51:34 +0100

    Add bubble sort test

--- src/main/scala/Sort.scala
+++ src/main/scala/Sort.scala
@@ -3,9 +3,14 @@
   */
 object Sort {
   def bubble[T](list: List[T])(implicit ord: Ordering[T]): List[T] = {
-    for(
-      i ← list;
-      j ← list
-    ) yield ord.max(i,j)
+    def go(list: List[T]): (List[T], Boolean) = list match {
+      case it1 :: it2 :: tail if ord.lt(it1, it2) ⇒ (it2 :: go((it1 :: tail))._1, true)
+      case it1 :: it2 :: tail if ord.gt(it1, it2) ⇒ (it1 :: go((it2 :: tail))._1, false)
+      case head :: Nil ⇒ (head :: Nil, false)
+    }
+    go(list) match {
+      case (ls, true) ⇒ go(ls)._1
+      case (ls, false) ⇒ ls
+    }
   }
 }
---
+++ src/test/scala/SortSpec.scala
@@ -0,0 +1,30 @@
+import org.specs2.Specification
+
+class SortSpec extends Specification {
+  def is =
+    s2"""
+       |Bubble sort
+       |  sort sorted list ${Bubble.sortSorted}
+       |  sort reversed list ${Bubble.sortReversed}
+       |  sort randomed list ${Bubble.sortRandomed}
+       |  sort empty list ${}
+     """.stripMargin
+
+  val sorted = List(6,5,4,3,2,1)
+  val reversed = sorted.reverse
+  val randomed = List(3,4,5,2,1,6)
+  val empty = List.empty[Int]
+  val single = List(1)
+
+  object Bubble {
+    def sortSorted = Sort.bubble(sorted) mustEqual (sorted)
+
+    def sortReversed = Sort.bubble(reversed) mustEqual (sorted)
+
+    def sortRandomed = Sort.bubble(randomed) mustEqual (sorted)
+
+    def sortEmpty = Sort.bubble(empty) mustEqual (empty)
+
+    def sortSingle = Sort.bubble(single) mustEqual (single)
+  }
+}
